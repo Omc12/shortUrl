bc453f73530f17514e98cc4338939c0675c955cd
Om <omchimurkar10@gmail.com>
2025-04-21 19:08:42 +0530

    added get, post, analytics

--- controllers/url.js
+++ controllers/url.js
@@ -3,11 +3,29 @@ const URL = require('../models/url')
 const { error } = require('console')
 
 async function handleGenerateNewShortUrl(req, res) {
-    const body = request.body
-    if (!body.url) return res.status(400).json({ error: 'ur; is required'})
+    const body = req.body
+    if (!body.url) return res.status(400).json({ error: 'url is required'})
     const shortID = nanoid(8)
     await URL.create({
         shortId: shortID,
         redirectUrl: body.url,
+        visitHistory: [],
     })
+
+    return res.json({ id: shortID })
+}
+
+async function handleGetAnalytics(req, res) {
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({ shortId })
+
+    return res.json({ 
+        totalClicks: result.visitHistory.length,
+        analytics: result.visitHistory,
+    })
+}
+
+module.exports = {
+    handleGenerateNewShortUrl,
+    handleGetAnalytics,
 }
