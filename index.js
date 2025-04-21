31c2e12cdd1c0d3bc0b270fb7cbac3b725b0370a
Om <omchimurkar10@gmail.com>
2025-04-21 19:56:30 +0530

    added history and a bit of frontend

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const express = require('express')
+const path = require('path')
 const urlRoute = require('./routes/url')
 const URL = require('./models/url')
 const { connectToMongoDB } = require('./connect')
-const { mongo } = require('mongoose')
-const { timeStamp } = require('console')
+const staticRouter = require('./routes/staticRouter')
+
 const app = express()
 const PORT = 8001
 
 connectToMongoDB('mongodb://localhost:27017/short-url')
 .then(() => console.log('mongoDB connected'))
 
-app.use(express.json())
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'))
 
-app.use('/url', urlRoute)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/:shortId', async (req, res) => {
+app.use('/url', urlRoute);
+app.use('/', staticRouter)
+
+app.get('/test', async(req, res) => {
+    const allUrls = await URL.find({})
+    return res.render('home', {
+        urls: allUrls,
+    })
+});
+
+app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
    const entry =  await URL.findOneAndUpdate(
     {
@@ -25,8 +38,8 @@ app.get('/:shortId', async (req, res) => {
             timestamp: Date.now()
         }
     },
-})
+});
 res.redirect(entry.redirectUrl)
-})
+});
 
-app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
