27e5a86c02aad536488e6d01ca0e9c1d65bbd7b6
Om <omchimurkar10@gmail.com>
2025-06-08 11:08:34 +0530

    authorization

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const cookieParser = require('cookie-parser')
 
-const { restrictToLoggedinUserOnly, checkAuth } = require('./middlewares/auth')
+const { checkForAuthentication, restrictTo } = require('./middlewares/auth')
 
 const URL = require('./models/url')
 
@@ -25,10 +25,11 @@ app.set('views', path.resolve('./views'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(checkForAuthentication)
 
-app.use('/url', restrictToLoggedinUserOnly, urlRoute);
+app.use('/url', restrictTo(["NORMAL", "ADMIN"]), urlRoute);
 app.use('/user', userRoute);
-app.use('/', checkAuth, staticRouter);
+app.use('/', staticRouter);
 
 app.get('/test', async(req, res) => {
     const allUrls = await URL.find({})
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,31 +1,28 @@
+const { response } = require("express");
 const { getUser } = require("../service/auth")
 
-async function restrictToLoggedinUserOnly(req, res, next) {
-    const userUid = req.headers["authorization"];
-
-    if (!userUid) {
-        return res.redirect("/login")
-    }
-
-    const token = userUid.split("Bearer ")[1]; 
-    const user = getUser(token);
-
-    if (!user) return res.redirect("/login")
+function checkForAuthentication(req, res, next) {
+    const tokenCookie = req.cookies?.token;
+    req.user = null;
+    if (!tokenCookie) return next()
+    
+    const token = tokenCookie;
+    const user = getUser(token)
 
     req.user = user;
-    next();
+    return next()
 }
 
-async function checkAuth(req, res, next) {
-    const userUid = req.headers["authorization"];
-    const token = userUid ? userUid.split("Bearer ")[1] : null; 
-    const user = getUser(token);
+function restrictTo(roles) {
+    return function (req, res, next) {
+        if (!req.user) return res.redirect("/login")
+        if (!roles.includes(req.user.role)) return res.end("You are not authorized to access this resource")
 
-    req.user = user;
-    next();
+        return next()
+    }
 }
 
 module.exports = {
-    restrictToLoggedinUserOnly,
-    checkAuth,
+    checkForAuthentication,
+    restrictTo,
 }
